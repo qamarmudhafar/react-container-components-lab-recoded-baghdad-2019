c246b407537728106d9b8fa794ccd95e0672dfe6
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-17 15:50:51 +0000

    Automatically backed up by Learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -7,3 +7,24 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends Component {
+  state = {
+      reviews: []
+    };
+
+
+  componentDidMount() {
+    fetch(URL)
+    .then(res=>res.json())
+    .then(json=> this.setState({
+      reviews:json.results
+    }))
+  }
+
+  render (){
+    return(
+      <div className="latest-movie-reviews"><MovieReviews reviews={this.state.reviews} /></div>
+    )
+  }
+}
+  export default LatestMovieReviewsContainer;
